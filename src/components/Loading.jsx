3e7df167e43f2b4e1a543057c240a63760b302bc
Harsh.S <shindeharsh0702@gmail.com>
2024-12-10 16:40:06 +0530

    Maintitle css added and position changed

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Loading.css'; // Import CSS for the loading spinner
+import './maintitle.css';
 
 const Loading = () => {
   return (
+    
     <div className="loading-container">
+      <b> <h2 class="Maintitle">Phygital Farms</h2></b>
       <div className="loading-spinner"></div>
       <p>Loading...</p>
-      <b> Phygital Farms</b>
+      
     </div>
   );
 };
