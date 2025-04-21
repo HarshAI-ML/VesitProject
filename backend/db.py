3ddabfd7a26fba250b3a654963d05098b498a903
Harsh.S <shindeharsh0702@gmail.com>
2025-04-21 21:46:41 +0530

    files restructured

--- backend/db.py
+++ backend/db.py
@@ -4,5 +4,5 @@ from mongoengine import connect
 # Replace with your actual MongoDB URI
 connect(
     db="your_database_name",
-    host="mongodb+srv://<username>:<password>@<cluster-url>/your_database_name?retryWrites=true&w=majority"
+    host="mongodb+srv://<username>:<password>@<cluster-url>/your_database_name?retryWrites=true&w=majority",
 )
